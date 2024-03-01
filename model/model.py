9d5de39c388a3d8bdcf03d5c3ac1cb39fcea9c26
nordengt <nordenghisingt123@gmail.com>
2024-03-01 21:19:47 +0545

    norm_pool + padding

--- model/model.py
+++ model/model.py
@@ -7,15 +7,16 @@ class ConvBlock(nn.Module):
         in_channels: int, 
         out_channels: int, 
         kernal_size: int, 
-        stride: int = 1, 
-        norm: bool = False, 
-        pool: bool = False
+        padding: int = 1, 
+        stride: int = 1,
+        norm_pool: bool = False, 
     ) -> None:
         super().__init__()
         layers = []
-        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=kernal_size, stride=stride))
-        if norm: layers.append(nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2))
-        if pool: layers.append(nn.MaxPool2d(kernel_size=3, stride=2))
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=kernal_size, padding=padding, stride=stride))
+        if norm_pool: 
+            layers.append(nn.LocalResponseNorm(size=5, alpha=1e-4, beta=0.75, k=2))
+            layers.append(nn.MaxPool2d(kernel_size=3, stride=2))
         layers.append(nn.ReLU())
         self.conv_block = nn.Sequential(*layers)
     
@@ -36,11 +37,11 @@ class LinearBlock(nn.Module):
 class AlexNet(nn.Module):
     def __init__(self, in_channels: int, num_classes: int) -> None:
         super().__init__()
-        self.conv1 = ConvBlock(in_channels, 96, kernal_size=11, stride=4, norm=True, pool=True)
-        self.conv2 = ConvBlock(96, 256, kernal_size=5, norm=True, pool=True)
+        self.conv1 = ConvBlock(in_channels, 96, kernal_size=11, padding=2, stride=4, norm_pool=True)
+        self.conv2 = ConvBlock(96, 256, kernal_size=5, padding=2, norm_pool=True)
         self.conv3 = ConvBlock(256, 384, kernal_size=3)
-        self.conv4 = ConvBlock(384, 256, kernal_size=3)
-        self.conv5 = ConvBlock(256, 256, kernal_size=3, pool=True)
+        self.conv4 = ConvBlock(384, 384, kernal_size=3)
+        self.conv5 = ConvBlock(384, 256, kernal_size=3, norm_pool=True)
         self.fc1 = LinearBlock(256*6*6, 4096)
         self.fc2 = LinearBlock(4096, 4096)
         self.fc3 = LinearBlock(4096, num_classes, dropout=False)
@@ -51,8 +52,18 @@ class AlexNet(nn.Module):
         x = self.conv3(x)
         x = self.conv4(x)
         x = self.conv5(x)
-        x = torch.flatten(x, start_dim=1)
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
         return x
+
+def main():
+    from torchinfo import summary
+    model = AlexNet(3, 1)
+    # print(model)
+    summary(model, input_size=[1, 3, 224, 224])
+
+if __name__ == "__main__":
+
+    main()
